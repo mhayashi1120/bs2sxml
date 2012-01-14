a02dc73a10be7a1001848a12950d7221de86e0ce
Masahiro Hayashi <mhayashi1120@gmail.com>
2012-01-14 14:07:49 +0900

    fix revert `write' to `display' orz add new procedure `read-file/sxml'

--- gauche/net/BeautifulSoup.scm
+++ gauche/net/BeautifulSoup.scm
@@ -1,14 +1,19 @@
 (define-module net.BeautifulSoup
+  (use gauche.charconv)
+  (use gauche.process)
   (use rfc.http)
   (use rfc.uri)
-  (use gauche.process)
   (export 
-   read-url/sxml read-file/sxml
+   read/sxml read-url/sxml read-file/sxml
    ))
 (select-module net.BeautifulSoup)
 
-(define (read-file/sxml file)
-  (call-bs2sxml (open-input-file file)))
+(define (read-file/sxml file :key (encoding #f))
+  (let* ([fp (open-input-file file :encoding encoding)])
+    (call-bs2sxml fp)))
+
+(define (read/sxml :optional (port (current-input-port)))
+  (call-bs2sxml port))
 
 (define (read-url/sxml url)
   (receive (proto a host b path . rest) 
@@ -16,15 +21,16 @@
     (receive (status headers body)
         (http-get host path)
       (receive (ip op) (sys-pipe)
-        (write body op)
+        (display body op)
         (close-output-port op)
         (let1 sxml (call-bs2sxml ip)
           (values sxml status headers))))))
 
-(define (call-bs2sxml input-port)
-  (let1 p (run-process `(bs2sxml) :input input-port :output 'out)
+(define (call-bs2sxml iport)
+  (let1 p (run-process `(bs2sxml) :input iport :output 'out)
     (process-wait p)
     (unless (eq? (process-exit-status p) 0)
-      (error "failed exit bs2sxml"))
+      (error "failed exit bs2sxml"
+             (port->string (process-output p 'out))))
     (with-input-from-port (process-output p 'out)
       read)))
