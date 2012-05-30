4d3a07546c5a3ace60f32db70352eef57a35e262
Masahiro Hayashi <mhayashi1120@gmail.com>
2012-05-30 23:10:53 +0900

    parameterize bs2sxml external command interpret special html character

--- gauche/net/BeautifulSoup.scm
+++ gauche/net/BeautifulSoup.scm
@@ -1,4 +1,5 @@
 (define-module net.BeautifulSoup
+  (use gauche.parameter)
   (use gauche.threads)
   (use gauche.charconv)
   (use gauche.process)
@@ -6,31 +7,43 @@
   (use rfc.uri)
   (export
    read/sxml read-url/sxml read-file/sxml
-   ))
+   bs2sxml-command))
 (select-module net.BeautifulSoup)
 
+(define bs2sxml-command
+  (make-parameter "bs2sxml"))
+
 (define (read-file/sxml file :key (encoding #f))
-  (let* ([fp (open-input-file file :encoding encoding)])
-    (call-bs2sxml fp)))
+  ;; TODO encoding is obsoleted investigate about encoding
+  (let1 fp (open-input-file file)
+    (unwind-protect
+     (call-bs2sxml fp)
+     (close-output-port fp))))
 
 (define (read/sxml :optional (port (current-input-port)))
   (call-bs2sxml port))
 
-(define (read-url/sxml url)
-  (receive (proto server path)
+(define (read-url/sxml url :optional (post-data #f))
+  (receive (secure? server path)
       (parse-uri url)
     (receive (status headers body)
-        (http-get server path
-                  :secure (string=? proto "https"))
+        (if post-data
+          (http-post server path post-data)
+          (http-get server path
+                    :secure secure?))
       (receive (ip op) (sys-pipe)
-        (let1 th (make-thread 
-                  (^()
-                    (display body op)
-                    (close-output-port op)))
-          (thread-start! th)
-          (let1 sxml (call-bs2sxml ip)
-            (thread-join! th)
-            (values sxml status headers)))))))
+        (unwind-protect
+         (let1 th (make-thread
+                   (^()
+                     (display body op)
+                     (close-output-port op)))
+           (thread-start! th)
+           (let1 sxml (call-bs2sxml ip)
+             (thread-join! th)
+             (values sxml status headers)))
+         (begin
+           (close-input-port ip)
+           (close-output-port op)))))))
 
 (define (parse-uri uri)
   (receive (proto user host port path query . rest)
@@ -44,10 +57,10 @@
                     #`",|path|?,|query|"]
                    [path path]
                    [else "/"])])
-      (values proto server htpath))))
+      (values (string=? proto "https") server htpath))))
 
 (define (call-bs2sxml iport)
-  (let* ([p (run-process `(bs2sxml) :input iport :output 'out)]
+  (let* ([p (run-process `(,(bs2sxml-command)) :input iport :output 'out)]
          [out (port->string (process-output p 'out))])
     (begin0
       (with-input-from-string out read)
