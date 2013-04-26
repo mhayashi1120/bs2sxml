0072d25ea82a5bfa4b9d31e5af1b0a4da6e03da2
Masahiro Hayashi <mhayashi1120@gmail.com>
2013-04-27 00:14:40 +0900

    print error to stderr fix handling text nodes

--- test.scm
+++ test.scm
@@ -4,6 +4,7 @@
 
 (use net.BeautifulSoup)
 (use sxml.sxpath)
+(use srfi-13)
 (test-module 'net.BeautifulSoup)
 
 (debug-print-width #f)
@@ -12,7 +13,18 @@
   (test* "Normal title"
          "Title"
          ((if-car-sxpath '(html head title *text*)) sxml))
-  )
+  (test* "Normal title"
+         '("Header" "Footer")
+         (map string-trim-both ((sxpath '(html body *text*)) sxml)))
+  (test* "Normal attribute"
+         "hoge"
+         ((if-car-sxpath '(html body div @ id *text*)) sxml)))
+
+;;TODO
+;; (let1 sxml (read-file/sxml "test/invalid1.htm")
+;;   sxml)
+
+
 
 
 (test-end)
